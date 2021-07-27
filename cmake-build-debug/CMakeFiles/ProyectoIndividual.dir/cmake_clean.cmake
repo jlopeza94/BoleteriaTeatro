19050d9e41b7e882e51b916adc86ea30897ae49b
Jlopez <jlopeza@ucenfotec.ac.cr>
2021-07-27 16:55:28 -0600

    Inicial

---
+++ cmake-build-debug/CMakeFiles/ProyectoIndividual.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoIndividual.dir/library.cpp.obj"
+  "ProyectoIndividual.lib"
+  "ProyectoIndividual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoIndividual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
